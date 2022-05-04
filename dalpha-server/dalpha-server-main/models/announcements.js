a867b0e8efabf930e012d64512b58778154d52fc
Gunjan Haldar <52736705+gun-py@users.noreply.github.com>
2022-05-04 12:16:18 +0530

    Delete dalpha-server/dalpha-server-main directory

--- dalpha-server/dalpha-server-main/models/announcements.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-    _id: String,
-    name: String,
-    announcements: Object,
-    fraud: Number
-})
-
-module.exports = mongoose.model('Announcements', schema, 'companies');
--- dalpha-server/dalpha-server-main/utils/textFilter.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (text) {
-    text = text
-        .replace(/[^ -~\n]+/g, "")
-        .replace(/\n[\n]+/g, "\n\n")
-    return text
-}
